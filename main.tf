4dc260f14e6dca2a227de81428fc64185336dd58
Raj-Hcl <raj.tripathi@hcltech.com>
2026-03-22 17:11:02 +0530

    Add Terraform configuration for Google Cloud VM

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+  credentials = var.credentials_json
+}
+
+resource "google_compute_instance" "default" {
+  name         = "my-vm"
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
